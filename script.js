efd77aeb74a16158e1a8c3296cb07262f4b5c719
abrahamnyuiadzi <abrahamnyuiadzi@gmail.com>
2023-06-06 17:29:16 +0000

    dom assingment

---
+++ script.js
@@ -0,0 +1,16 @@
+const link = document.querySelector("ul");
+
+const input = document.querySelector("input");
+
+
+
+const button = document.querySelector("button");
+button.addEventListener("click",function(e) {
+    const script =prompt("input")
+    console.log(script)
+  
+
+    document.body.innerHTML= " <value>" 
+}
+
+)
